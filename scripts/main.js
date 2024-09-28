d57e0939e7354f23dffe6d44252fedb74dfef660
Katerina <esidor27@ya.ru>
2024-09-28 19:29:41 +0300

    random

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('.form');
+const numberShow = document.querySelector('.show-number');
+const minInput = document.querySelector('.field-min');
+const maxInput = document.querySelector('.field-max');
+
+
+
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //[0, 1]
+}
+
+//Получаем значения от input
+function updateNumberShow() {
+    const min = parseInt(minInput.value, 10);
+    const max = parseInt(maxInput.value, 10);
+    const randomNumber = getRandomNumber(min, max);
+    numberShow.textContent = randomNumber;
+}
+
+form.addEventListener('submit', () => {
+    event.preventDefault();
+
+    setTimeout(() => {
+        updateNumberShow();
+    }, 5000);
+})
+
